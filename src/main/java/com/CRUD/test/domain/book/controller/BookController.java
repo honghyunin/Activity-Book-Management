4fcc110abe6d50cba30ad492ad3c2439ed45cb2f
hyunin <68847615+honghyunin@users.noreply.github.com>
2021-12-03 01:24:47 +0900

    (BookController) 생성

---
+++ src/main/java/com/CRUD/test/domain/book/controller/BookController.java
@@ -0,0 +1,11 @@
+package com.CRUD.test.domain.book.controller;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/api")
+@RequiredArgsConstructor
+@RestController
+public class BookController {
+
+}
