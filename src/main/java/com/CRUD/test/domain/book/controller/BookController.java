a874876e23b226b4958082874ad1a0ddb52800b9
hyunin <68847615+honghyunin@users.noreply.github.com>
2021-12-03 01:25:13 +0900

    (BookController) Book Add

--- src/main/java/com/CRUD/test/domain/book/controller/BookController.java
+++ src/main/java/com/CRUD/test/domain/book/controller/BookController.java
@@ -1,5 +1,11 @@
 package com.CRUD.test.domain.book.controller;
+import com.CRUD.test.domain.book.dto.BookRequestDto;
+import com.CRUD.test.domain.book.service.BookService;
+import com.CRUD.test.global.response.ResponseService;
+import com.CRUD.test.global.response.SingleResult;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,4 +14,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class BookController {
 
+    private final ResponseService responseService;
+    private final BookService bookService;
+
+    @PostMapping("/add")
+    public SingleResult<Long> addBook(@RequestBody BookRequestDto.Add book) {
+        return responseService.getSingleResult(bookService.addBook(book));
+    }
 }
