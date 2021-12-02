68e5540c08c8b3db01119333dd6451a3c77e598b
hyunin <68847615+honghyunin@users.noreply.github.com>
2021-12-02 16:48:50 +0900

    (UserRepository) findById 메소드 반환형에 Optional 추가

--- src/main/java/com/CRUD/test/repository/UserRepository.java
+++ src/main/java/com/CRUD/test/repository/UserRepository.java
@@ -4,10 +4,11 @@ import com.CRUD.test.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
 
 @Repository //퍼시스턴스 레이어나, DB나 파일같은 외부 I/O작업을 처리할 때 쓰이는 어노테이션
 public interface UserRepository extends JpaRepository<User,Long> {
-    public User findById(String Id);
-    public User findByPw(String pw);
-    public Long findByIdx(String id);
+    Optional<User> findById(String Id);
+
+    void delete(User user);
 }
