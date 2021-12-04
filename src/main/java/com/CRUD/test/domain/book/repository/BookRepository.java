ae38b9b4edb977723c8fbcb66523eaf4cfd39ce8
hyunin <68847615+honghyunin@users.noreply.github.com>
2021-12-04 23:43:20 +0900

    (BookRepository) Service logic 에 필요한 Jpa Method 생성

--- src/main/java/com/CRUD/test/domain/book/repository/BookRepository.java
+++ src/main/java/com/CRUD/test/domain/book/repository/BookRepository.java
@@ -4,9 +4,12 @@ import com.CRUD.test.domain.book.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
+import java.util.List;
 
 @Repository
 public interface BookRepository extends JpaRepository<Book, Long> {
-    Book findByTitle(String title);
+    Book findByIdx(Long idx);
+    List<Book> findByTitle(String title);
+    List<Book> findByCategory(String category);
+    List<Book> findByRentalAbleBook(Boolean rentalAbleBook);
 }
