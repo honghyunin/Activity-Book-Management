331392dc8d7c287fe10b04841c6da2e6dac5a89c
hyunin <68847615+honghyunin@users.noreply.github.com>
2021-12-04 23:31:02 +0900

    (BookService) update 구현

--- src/main/java/com/CRUD/test/domain/book/service/BookService.java
+++ src/main/java/com/CRUD/test/domain/book/service/BookService.java
@@ -1,9 +1,11 @@
 package com.CRUD.test.domain.book.service;
 
+import com.CRUD.test.domain.book.Book;
 import com.CRUD.test.domain.book.dto.BookRequestDto;
 import com.CRUD.test.domain.book.repository.BookRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @RequiredArgsConstructor
 @Service
@@ -12,9 +14,18 @@ public class BookService {
     private final BookRepository bookRepository;
 
     public Long addBook(BookRequestDto.Add book){
-        if(bookRepository.findByTitle(book.getTitle()) != null){
-            throw new RuntimeException("이미 존재하는 책입니다");
-        }
         return bookRepository.save(book.toEntity()).getIdx();
+    }
+
+    @Transactional
+    public String updateBook(BookRequestDto.Add book){
+        if(bookRepository.findByIdx(book.getIdx()) == null){
+            throw new RuntimeException("존재하지 않는 책입니다");
+        }
+        Book books = bookRepository.findById(book.getIdx())
+                .orElseThrow(IllegalArgumentException::new);
+
+        books.update(book.getTitle(), book.getCategory(), book.getAuthor());
+        return "책 정보 수정이 완료되었습니다";
     }
 }
