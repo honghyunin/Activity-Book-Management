ed450ff6ad76e12553460484b0e7e55655ba3d7f
hyunin <68847615+honghyunin@users.noreply.github.com>
2021-12-05 00:15:01 +0900

    (RentalController) 대여 할 수 있는 책 조회 기능 컨트롤러

--- src/main/java/com/CRUD/test/domain/rentalbook/controller/RentalController.java
+++ src/main/java/com/CRUD/test/domain/rentalbook/controller/RentalController.java
@@ -1,12 +1,15 @@
 package com.CRUD.test.domain.rentalbook.controller;
 
+import com.CRUD.test.domain.book.Book;
 import com.CRUD.test.domain.rentalbook.dto.RentalReqDto;
 import com.CRUD.test.domain.rentalbook.service.RentalService;
 import com.CRUD.test.global.response.CommonResult;
 import com.CRUD.test.global.response.ResponseService;
+import com.CRUD.test.global.response.SingleResult;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 
 @RequestMapping("/api/rental")
 @RequiredArgsConstructor
@@ -20,5 +23,10 @@ public class RentalController {
     public CommonResult rental(@RequestBody RentalReqDto.Rental book){
         rentalBookService.rentalBook(book);
         return responseService.getSuccessResult();
+    }
+
+    @GetMapping("/read")
+    public SingleResult<List<Book>> rentalSelect(){
+        return responseService.getSingleResult(rentalBookService.selectRentalAbleBook());
     }
 }
