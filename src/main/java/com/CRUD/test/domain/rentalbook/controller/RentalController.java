4853d6c8ba044d92e85c4216833aa84db6697c83
hyunin <68847615+honghyunin@users.noreply.github.com>
2021-12-05 00:13:33 +0900

    (RentalController) 생성

---
+++ src/main/java/com/CRUD/test/domain/rentalbook/controller/RentalController.java
@@ -0,0 +1,11 @@
+package com.CRUD.test.domain.rentalbook.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RequestMapping("/api/rental")
+@RequiredArgsConstructor
+@RestController
+public class RentalController {
+
+}
