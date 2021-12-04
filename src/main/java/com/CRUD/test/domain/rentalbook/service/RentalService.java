073f2c771a4ad861014cb86ab9e3ec53c7de9bc5
hyunin <68847615+honghyunin@users.noreply.github.com>
2021-12-05 00:11:16 +0900

    (RentalService) 생성

---
+++ src/main/java/com/CRUD/test/domain/rentalbook/service/RentalService.java
@@ -0,0 +1,10 @@
+package com.CRUD.test.domain.rentalbook.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class RentalService {
+
+}
