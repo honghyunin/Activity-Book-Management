de9d7eb6aa8bade4ee04cae8d3050ff32946768f
hyunin <68847615+honghyunin@users.noreply.github.com>
2021-12-05 00:14:31 +0900

    (RentalController) 대여 기능 컨트롤러

--- src/main/java/com/CRUD/test/domain/rentalbook/controller/RentalController.java
+++ src/main/java/com/CRUD/test/domain/rentalbook/controller/RentalController.java
@@ -1,11 +1,24 @@
 package com.CRUD.test.domain.rentalbook.controller;
 
+import com.CRUD.test.domain.rentalbook.dto.RentalReqDto;
+import com.CRUD.test.domain.rentalbook.service.RentalService;
+import com.CRUD.test.global.response.CommonResult;
+import com.CRUD.test.global.response.ResponseService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
+
 
 @RequestMapping("/api/rental")
 @RequiredArgsConstructor
 @RestController
 public class RentalController {
 
+    private final ResponseService responseService;
+    private final RentalService rentalBookService;
+
+    @PostMapping("/")
+    public CommonResult rental(@RequestBody RentalReqDto.Rental book){
+        rentalBookService.rentalBook(book);
+        return responseService.getSuccessResult();
+    }
 }
