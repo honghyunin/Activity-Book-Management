688a6c75970ab39b93b461466a239791e2237229
hyunin <68847615+honghyunin@users.noreply.github.com>
2021-12-05 00:16:54 +0900

    (RentalReqDto) Rental Dto 생성

--- src/main/java/com/CRUD/test/domain/rentalbook/dto/RentalReqDto.java
+++ src/main/java/com/CRUD/test/domain/rentalbook/dto/RentalReqDto.java
@@ -1,14 +1,27 @@
 package com.CRUD.test.domain.rentalbook.dto;
 
+import com.CRUD.test.domain.book.Book;
+import com.CRUD.test.domain.user.User;
 import lombok.Getter;
 import lombok.Setter;
+
+import java.time.LocalDate;
 
 public class RentalReqDto {
 
     @Getter
     @Setter
     public static class Rental { // 대여
+        private Long bookIdx;
 
+        public com.CRUD.test.domain.rentalbook.Rental toEntity(User user, Book book){
+            return com.CRUD.test.domain.rentalbook.Rental.builder()
+                    .user(user)
+                    .book(book)
+                    .rental_date(LocalDate.now())
+                    .return_date(LocalDate.now().plusDays(7))
+                    .build();
+        }
     }
 
     @Getter
