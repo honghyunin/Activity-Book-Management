8e628ccd94cd0486d302380fbb417837328f77e9
hyunin <68847615+honghyunin@users.noreply.github.com>
2021-12-07 15:55:32 +0900

    (UserRepository) idx를 통해 User를 찾는 메소드 추가

--- src/main/java/com/crud/test/model/user/repository/UserRepository.java
+++ src/main/java/com/crud/test/model/user/repository/UserRepository.java
@@ -1,6 +1,6 @@
-package com.CRUD.test.repository;
+package com.crud.test.model.user.repository;
 
-import com.CRUD.test.domain.User;
+import com.crud.test.model.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -9,6 +9,6 @@ import java.util.Optional;
 @Repository //퍼시스턴스 레이어나, DB나 파일같은 외부 I/O작업을 처리할 때 쓰이는 어노테이션
 public interface UserRepository extends JpaRepository<User,Long> {
     Optional<User> findById(String Id);
-
+    Optional<User> findById(Long idx);
     void delete(User user);
 }
