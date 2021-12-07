f84a44d7b5d79822844e30a42f83ef7fa96ee5ff
hyunin <68847615+honghyunin@users.noreply.github.com>
2021-12-07 15:51:48 +0900

    (UserService, Impl) delete 메소드 반환 타입 변경

--- src/main/java/com/crud/test/model/user/service/Impl/UserServiceImpl.java
+++ src/main/java/com/crud/test/model/user/service/Impl/UserServiceImpl.java
@@ -1,41 +1,42 @@
-package com.CRUD.test.Service.Impl;
+package com.crud.test.model.user.service.Impl;
 
-import com.CRUD.test.Service.EmailSenderService;
-import com.CRUD.test.Service.UserService;
-import com.CRUD.test.advice.exception.UserNotFoundException;
-import com.CRUD.test.advice.exception.UserNotmatch;
-import com.CRUD.test.domain.User;
-import com.CRUD.test.dto.*;
-import com.CRUD.test.repository.UserRepository;
-import com.CRUD.test.security.JwtTokenProdvider;
-import com.CRUD.test.util.CurrentUserUtil;
-import com.CRUD.test.util.RedisUtil;
+import com.crud.test.model.book.Book;
+import com.crud.test.model.rental.repository.RentalCustomRepository;
+import com.crud.test.model.rental.repository.RentalRepository;
+import com.crud.test.model.user.service.UserService;
+import com.crud.test.global.advice.exception.UserNotFoundException;
+import com.crud.test.global.advice.exception.UserNotmatch;
+import com.crud.test.model.user.User;
+import com.crud.test.model.user.dto.*;
+import com.crud.test.model.user.repository.UserRepository;
+import com.crud.test.global.security.JwtTokenProdvider;
+import com.crud.test.global.util.CurrentUserUtil;
+import com.crud.test.global.util.RedisUtil;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
-
 
 @RequiredArgsConstructor
 @Service
 public class UserServiceImpl implements UserService {
     private final UserRepository userRepository;
+    private final RentalRepository rentalRepository;
     private final PasswordEncoder passwordEncoder;
     private final JwtTokenProdvider jwtTokenProdvider;
     private final RedisUtil redisUtil;
-    private final EmailSenderService emailSenderService;
     private final CurrentUserUtil currentUserUtil;
+    private final RentalCustomRepository rentalCustomRepository;
 
     @Transactional
     public Long signup(UserSaveRequestDto user) {
         if(userRepository.findById(user.getId()).isPresent()){
             throw new IllegalArgumentException("유저를 찾을 수 없습니다");
         }
-
-        emailSenderService.sendEmail(user); // 회원가입을 했을 경우 이메일 인증을 통해 인증된 유저로써 서비스를 이용할 수 있음
 
         user.setPw(passwordEncoder.encode(user.getPw())); // password 암호화
 
@@ -45,7 +46,7 @@ public class UserServiceImpl implements UserService {
     public Map<String, String> login(UserLoginDto user){
 
         User findUser = userRepository.findById(user.getId())
-                .orElseThrow(() -> new IllegalArgumentException("유저를 찾을 수 없습니다"));
+                .orElseThrow(() -> new IllegalArgumentException("유저를 찾을 수 없습니다")); // 해당 예외처리 같은 경우는 프론트단에서 문제가 발생할 수 있으므로 추후 리팩토링 시 변경함
 
         if(!passwordEncoder.matches(user.getPw(), findUser.getPw()))
         {
@@ -55,13 +56,22 @@ public class UserServiceImpl implements UserService {
         String AccessToken = jwtTokenProdvider.createToken(user.getId(), user.toEntity().getRoles()); //, user.toEntity().getRoles()
         String RefreshToken = jwtTokenProdvider.createRefreshToken();
 
-        redisUtil.deleteData(user.getId()); // redis에 값을 삽입하기 전 해당 아이디 삭제
+        redisUtil.deleteData(user.getId()); // redis에 값을 삽입하기 전 해당 아이디 삭제하나용??
+        // redis에 값을 삽입하기전에 redis 내부에 있는 id 값을 삭제한다.
+        // 왜냐하면 하나의 유저는 하나의 토큰을 가지는 법칙이 존재하므로
+        // 유저가 로그아웃을 하지 않고 유효기간이 만료된 토큰이 생길 경우 법칙에 위배되며, 토큰이 여러 개가 생길 경우 토큰을 관리하는 과정에서 여러가지 문제점이 발생하므로
+        // 토큰을 삽입하기전에 key를 삭제하므로써 이를 방지한다.
         redisUtil.setDataExpire(user.getId(), RefreshToken, JwtTokenProdvider.REFRESH_TOKEN_VAILD_TIME);
 
         Map<String, String> map = new HashMap<>();
-        map.put("ID", user.getId());
+        map.put("ID", user.getId()); // map은 클라이언트에게 넘어가는 데이터이다. 그러므로 이 토큰을 어떤 유저가 가졌는 지 알기 위함.
+        // 굳이 추가하지 않아도 되지만 클라이언트와 상호 협의하에 결정해도 될 문제.
+
         map.put("AccessToken", "Bearer "+AccessToken);
         map.put("RefreshToken", "Bearer " +RefreshToken);
+        // 'Bearer'으로 토큰이 유효한지 인증한다.
+        // Token에 'Bearer'이 있을 경우 해당 토큰이 인증 됨을 나타내므로
+        // Bearer을 토큰에 추가해준다.
 
         return map;
     }
@@ -84,10 +94,13 @@ public class UserServiceImpl implements UserService {
         redisUtil.deleteData(currentUserUtil.getCurrentUser().getId());
     }
 
-    public String delete(Long idx) {
+    public Long delete(Long idx) {
         userRepository.deleteById(idx); // 내 localDB에 존재하는 idx의 열을 삭제함
-        return  idx +"is Delete" ;
+        return idx;
     }
 
-
+    public List<Book> userRentalBook(){
+        User user = currentUserUtil.getCurrentUser();
+        return rentalCustomRepository.findUserRental(user);
+    }
 }
--- src/main/java/com/crud/test/model/user/service/UserService.java
+++ src/main/java/com/crud/test/model/user/service/UserService.java
@@ -10,7 +10,7 @@ public interface UserService {
     Long signup(UserSaveRequestDto user); // 데이터를 local DB에 저장하는 메소드
     UserResponseDto findById(Long idx); // local DB안에 있는 데이터를 요청으로 들어온 Long타입의 idx값으로 찾는 메소드
     Long update(UserUpdateRequestDto requestDto); // idx 열에 있는 데이터를 요청으로 받아온 id와 pw의 값으로 변경해주는 메소드
-    String delete(Long idx); // idx값의 열을 삭제해주는 메소드(데이터 또한)
+    Long delete(Long idx); // idx값의 열을 삭제해주는 메소드(데이터 또한)
     Map<String, String> login(UserLoginDto user);
     void logout();
 }
