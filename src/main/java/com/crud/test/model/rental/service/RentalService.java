aef01e1545fc6e168b53ed6c060264b6702e9a74
hyunin <68847615+honghyunin@users.noreply.github.com>
2021-12-08 20:45:28 +0900

    (RentalService) 추가

---
+++ src/main/java/com/crud/test/model/rental/service/RentalService.java
@@ -0,0 +1,9 @@
+package com.crud.test.model.rental.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class RentalService {
+}
