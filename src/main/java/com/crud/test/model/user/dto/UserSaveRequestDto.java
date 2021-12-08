b7eb9fea53bf1ad38a49bea5a759aa889dbd4652
hyunin <68847615+honghyunin@users.noreply.github.com>
2021-12-08 20:42:51 +0900

    (UserSaveRequestDto) User의 Column 추가에 따른 Dto 필드 추가

--- src/main/java/com/crud/test/model/user/dto/UserSaveRequestDto.java
+++ src/main/java/com/crud/test/model/user/dto/UserSaveRequestDto.java
@@ -1,7 +1,7 @@
-package com.CRUD.test.dto;
+package com.crud.test.model.user.dto;
 
-import com.CRUD.test.domain.Role;
-import com.CRUD.test.domain.User;
+import com.crud.test.model.user.Role;
+import com.crud.test.model.user.User;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -13,11 +13,17 @@ import java.util.Collections;
 public class UserSaveRequestDto {
     private String id;
     private String pw;
+    private String name;
+    private String userNumber;
+    private Boolean rentalAbleUser = true;
 
     public User toEntity(){
         return User.builder()
                 .id(id)
                 .pw(pw)
+                .name(name)
+                .userNumber(userNumber)
+                .rentalAbleUser(rentalAbleUser)
                 .roles(Collections.singletonList(Role.ROLE_NOT_AUTH))
                 .build();
     }
